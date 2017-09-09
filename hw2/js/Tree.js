b4e3442a0c6063644bef80c23f879a1787261eba
Polina Kukhareva <polina.kukhareva@utah.edu>
2017-09-08 18:01:00 -0600

    hw2 completed

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -65,17 +65,49 @@ class Tree {
  		}
 	//Assign Positions and Levels by making calls to assignPosition() and assignLevel()
 
-	this.assignLevel(this.nodes[0], 0)
-	this.assignPosition(this.nodes[0], 0)
+	this.assignLevel(this.nodes[0], 0);
+	this.assignPosition(this.nodes[0], 0);
 	}
-
-
 
 	/**
 	 * Function that renders the tree
 	 */
 	renderTree() {
-
+		var treeArray=this.nodes;
+		var execute = function () {
+    let svg = d3.select("svg");
+ 		for (var j = 0; j<treeArray.length; j++){
+				var node = treeArray[j];
+				if (node.level > 0){
+				svg.append("line")
+								.attr("x1", 50+200*node.parentNode.level)
+								.attr("y1", 50+100*node.parentNode.position)
+								.attr("x2", 50+200*node.level)
+								.attr("y2", 50+100*node.position);
+				}
+    }
+		for (var j = 0; j<treeArray.length; j++){
+				var node = treeArray[j];
+				svg.append("circle")
+								.attr("cx", 50+200*node.level)
+								.attr("cy", 50+100*node.position)
+								.attr("r", 40);
 	}
+	for (var j = 0; j<treeArray.length; j++){
+			var node = treeArray[j];
+			svg.append("text")
+							.attr("x", 50+200*node.level)
+							.attr("y", 50+100*node.position)
+							.text(node.name)
+							.style("fill","white")
+							.style("font-weight","bold")
+							.style("text-anchor","middle")
+							.style("font-size","16px")
+							.style("letter-spacing","3px")
+							;
+}
+	}
+	execute();
 
+}
 }
--- hw2/js/script.js
+++ hw2/js/script.js
@@ -22,64 +22,6 @@ function fetchJSONFile(path, callback) {
 fetchJSONFile('data/Tree.json', function(data) {
 	let tree = new Tree(data);
 	tree.buildTree();
+	console.log(tree.nodes);
 	tree.renderTree();
 });
-var jsonData = [{
-"name":"Animal",
-"parent":"root"
-},
-{
-"name":"Reptile",
-"parent":"Animal"
-},
-{
-"name":"Mammal",
-"parent":"Animal"
-},
-{
-"name":"Lizard",
-"parent":"Reptile"
-},
-{
-"name":"Snake",
-"parent":"Reptile"
-},
-{
-"name":"Bird",
-"parent":"Reptile"
-},
-{
-"name":"Canary",
-"parent":"Bird"
-},
-{
-"name":"Equine",
-"parent":"Mammal"
-},
-{
-"name":"Bovine",
-"parent":"Mammal"
-},
-{
-"name":"Canine",
-"parent":"Mammal"
-},
-{
-"name":"Cow",
-"parent":"Bovine"
-},
-{
-"name":"Horse",
-"parent":"Equine"
-},
-{
-"name":"Zebra",
-"parent":"Equine"
-},
-{
-"name":"Dog",
-"parent":"Canine"
-}];
-let treeTest = new Tree(jsonData);
-treeTest.buildTree();
-console.log(treeTest.nodes);
